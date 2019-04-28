ae973f82e361f0171232a5fe8c713cbc6d91c345
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-04-28 23:40:32 +0400

    Add styles to the App

--- app.js
+++ app.js
@@ -5,36 +5,39 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-var items = ['Buy Food', 'Cook Food', 'Eat Food'];
+let items = ['Buy Food', 'Cook Food', 'Eat Food'];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended:true }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
 
-    var today = new Date();
+    let today = new Date();
 
-    var options = {
+    let options = {
         weekday: 'long',
         day: 'numeric',
         month: 'long'
     };
 
-    var day = today.toLocaleDateString('en-US', options);
+    let day = today.toLocaleDateString('en-US', options);
 
     res.render('list', { kindOfDay: day, newListItems: items }); 
 
 });
 
 app.post('/', (req, res) => {
-    var item = req.body.newItem;
+
+    let item = req.body.newItem;
 
     items.push(item);
     
-
     res.redirect('/');
+    
 });
+
 
 app.listen(3000, () => {
     console.log('Server started on port 3000.')

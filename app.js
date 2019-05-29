8c7c82350ab6b1cfef05d08ce78ecf6674ab781a
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-05-29 19:50:11 +0400

    Connect App to MongoDB and add items to it's data base

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
-//jshint esversion:6
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const date = require(__dirname + '/date.js');
+const mongoose = require('mongoose');
 
 const app = express();
-
-const items = ['Buy Food', 'Cook Food', 'Eat Food'];
-const workItems =[];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(express.static('public'));
 
+mongoose.connect('mongodb://localhost:27017/todolistDB', { useNewUrlParser: true });
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: 'Welcome to your todolist!'
+});
+
+const item2 = new Item({
+    name: 'Hit the + button to add a new item.'
+});
+
+const item3 = new Item({
+    name: '<-- Hit this to delete an item.'
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, err => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('Successfully saved default items!');
+    }
+});
+
+
 app.get('/', (req, res) => {
 
-    const day = date.getDate();
-
-    res.render('list', { listTitle: day, newListItems: items }); 
+    res.render('list', { listTitle: "Today", newListItems: items }); 
 
 });
 
@@ -50,7 +74,6 @@ app.post('/work', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about');
 });
-
 
 
 app.listen(3000, () => {

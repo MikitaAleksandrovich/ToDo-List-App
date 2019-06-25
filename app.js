6a4593e7c03ed8507e1d4665c5ce6993ba7c1bf0
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-06-25 19:49:42 +0400

    Connect Mongodb database to remote mono Atlas

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost:27017/todolistDB', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://admin-nikita:Test-123@cluster0-ry02b.mongodb.net/todolistDB', { useNewUrlParser: true });
 
 const itemsSchema = {
     name: String
